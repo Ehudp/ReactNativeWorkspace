11033ab3dce1887a4d9a78283b5ffb7795375bcd
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-08 15:49:19 +0200

    add album list

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,9 @@
 /** @format */
+//npm install eslint --save-dev
+//npm install --save-dev eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
