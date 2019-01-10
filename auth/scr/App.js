7ddaff5e1b937a466266669a2f3c637187a40f18
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-10 15:13:06 +0200

    working

--- auth/scr/App.js
+++ auth/scr/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
 
 class App extends Component {
+    // componentWillMount() {
+    //     firebase.initializeApp({
+    //         apiKey: "AIzaSyBR3PTfRI4pugdxGLfGXnmPSco0L400h5Y",
+    //         authDomain: "auth-292f1.firebaseapp.com",
+    //         databaseURL: "https://auth-292f1.firebaseio.com",
+    //         projectId: "auth-292f1",
+    //         storageBucket: "auth-292f1.appspot.com",
+    //         messagingSenderId: "678462291932"
+    //     });
+    // }
 
     render() {
         return (
             <View>
-                <Text>An App</Text>
+                <Header headerText="Authentication" />
+                <LoginForm />
             </View>
         );
     }
