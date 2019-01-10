98ae48d99baf884f287d159efb9decd65091a17b
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-10 08:18:27 +0200

    fix eslint

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <View>
-
+        <View style={styles.containerStyle} >
+            {props.children}
         </View>
     );
+};
+
+const styles = {
+    containerStyle: {
+        borderWidth: 1,
+        borderRadius: 2,
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 2,
+        elevation: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
 };
 
 export default Card;
