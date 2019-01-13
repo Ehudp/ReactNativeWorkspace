14b5b00d843d4d0cbca995ee905339b48fda02cd
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-13 15:19:59 +0200

    add firebase

--- auth/scr/components/LoginForm.js
+++ auth/scr/components/LoginForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
 
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed.' })
+                    });
+            });
+    }
 
     render() {
         return (
@@ -26,9 +39,9 @@ class LoginForm extends Component {
                         onChangeText={password => this.setState({ password })}
                     />
                 </CardSection>
-
+                <Text style={styles.errorTextStyle}>{this.state.error}</Text>
                 <CardSection>
-                    <Button >
+                    <Button onPress={this.onButtonPress}>
                         Log in
                     </Button>
                 </CardSection>
@@ -36,5 +49,14 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
 
 export default LoginForm;
