bb3922319a49660c1c45deee20626054e5618767
ehud pinhas <ehudpinhas@ehuds-mbp.scr.local>
2019-01-15 08:22:15 +0200

    fix libraryList

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import ListItem from './ListItem';
-class LibraryList extends Component {
 
-    renderItem = (library) => {
-        return <ListItem library={library} />
+class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow(library) {
+        return <ListItem library={library} />;
     }
 
     render() {
         return (
-            <FlatList
-                data={this.props.library}
-                renderItem={this.renderItem}
-                keyExtractor={(library) => library.id}
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
             />
         );
     }
 }
 
 const mapStateToProps = state => {
-    return { libraries: state.libraries }
+    return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
