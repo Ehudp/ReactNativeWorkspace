9f9283a0048fc51bef27bfef7de0ae1c0aced95b
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-01-28 13:51:05 +0200

    add delete and edit employee

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 import { Actions } from 'react-native-router-flux';
 
 const RouterComponent = () => {
@@ -28,6 +29,12 @@ const RouterComponent = () => {
                         key="employeeCreate"
                         component={EmployeeCreate}
                         title='Create Employee'
+
+                    />
+                    <Scene
+                        key="employeeEdit"
+                        component={EmployeeEdit}
+                        title='Edit Employee'
 
                     />
                 </Scene>
--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, ListView } from 'react-native'
+import { ListView } from 'react-native'
 import { employeesFetch } from '../actions'
+import ListItem from './ListItem';
+
 class EmployeeList extends Component {
 
     componentWillMount() {
@@ -22,17 +24,17 @@ class EmployeeList extends Component {
         this.dataSource = ds.cloneWithRows(employees);
     }
 
+    renderRow(employee) {
+        return <ListItem employee={employee} />;
+    }
     render() {
-        console.log(this.props);
+
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         )
     }
 }
