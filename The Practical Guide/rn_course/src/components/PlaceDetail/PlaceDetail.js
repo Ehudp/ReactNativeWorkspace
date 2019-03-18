528cf4f232dd95d9fe5d70b43ec3a1eafb3c0a31
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-03-18 10:54:21 +0200

    add native icons lib

--- The Practical Guide/rn_course/src/components/PlaceDetail/PlaceDetail.js
+++ The Practical Guide/rn_course/src/components/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { View, Modal, Image, Text, Button, StyleSheet } from "react-native";
-
+import {
+  View,
+  Modal,
+  Image,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 const placeDetail = props => {
   let modalContent = null;
   if (props.selectedPlace) {
@@ -21,7 +29,12 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteButton}>
+              <Icon size={30} name="ios-trash" color="red" />
+            </View>
+          </TouchableOpacity>
+
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -41,6 +54,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: "center"
   }
 });
 
