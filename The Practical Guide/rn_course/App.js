e53ade03fd349b04ca719b394a48bc7c91524a81
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-03-20 08:47:41 +0200

    add navigation android not working

--- The Practical Guide/rn_course/App.js
+++ The Practical Guide/rn_course/App.js
@@ -1,75 +1,11 @@
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import { connect } from "react-redux";
-import PlaceInput from "./src/components/PlaceInput/PlaceInput";
-import PlaceList from "./src/components/PlaceList/PlaceList";
-import PlaceDetails from "./src/components/PlaceDetail/PlaceDetail";
-import {
-  addPlace,
-  deletePlace,
-  selectPlace,
-  deselectPlace
-} from "./src/store/actions/index";
+import { Navigation } from "react-native-navigation";
+import AuthScreen from "./src/screens/Auth/Auth";
 
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-  };
-  placeSelectedHandler = key => {
-    this.props.onselectPlace(key);
-  };
+Navigation.registerComponent("awesome-place.AuthScreen", () => AuthScreen);
 
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
-
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetails
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClosed={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "awesome-place.AuthScreen",
+    title: "Login"
   }
 });
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onselectPlace: key => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
--- The Practical Guide/rn_course/index.js
+++ The Practical Guide/rn_course/index.js
@@ -9,12 +9,12 @@ import App from "./App";
 import { name as appName } from "./app.json";
 import configureStore from "./src/store/configureStore";
 
-const store = configureStore();
+// const store = configureStore();
 
-const RNRedux = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+// const RNRedux = () => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
 
-AppRegistry.registerComponent(appName, () => RNRedux);
+AppRegistry.registerComponent(appName, () => App);
