e3f836b715dd8ccc87a1f59be384e0a2eabc82d3
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2019-04-01 14:43:33 +0300

    add ts_app

---
+++ The Practical Guide/ts_app/App.tsx
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from "react";
+import { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { Hello } from "./components/hello";
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Hello name={"World"} enthusiasmLevel={1} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
