636bffc5eb6d8231e5e36edf60b4f436915d9013
ehudp <ehud.pinhas@gmail.com>
2019-10-22 15:36:36 +0300

    cod clean

--- test/test3/App.tsx
+++ test/test3/App.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface Props {
+  name: String;
+}
+
+interface State {
+  massage: String;
+}
+
+class MyComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { massage: 'hello' };
+  }
+
+  componentDidMount() {}
+  render() {
+    console.log(`${this.state.massage} ${this.props.name}`);
+
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>MyComponent</Text>
+      </View>
+    );
+  }
+}
+
+var MyFunc = function() {
+  console.log('MyFunc');
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app</Text>
+      <Text>MyFunc</Text>
+    </View>
+  );
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <MyFunc />
+      <MyComponent name="MyComponent" />
     </View>
   );
 }
